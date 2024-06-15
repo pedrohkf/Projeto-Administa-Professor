64c1cb0eb412094f72fe4e99cf2380510d55af88
Pedro Henrique <phkfaria@gmail.com>
2024-06-15 15:54:51 -0300

    pagina inicial

---
+++ index.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow} = require('electron');
+const path = require('node:path');
+
+const createWindow = () => {
+    const window = new BrowserWindow({
+        width: 1920,
+        height: 1080,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    window.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin') app.quit()
+})
